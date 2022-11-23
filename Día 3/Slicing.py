ff21956da9de8cc0eb448dff72a8dc6516e9ad1a
Adrian <adrivara2018@gmail.com>
2022-11-22 20:02:11 -0600

    Slicing

---
+++ Día 3/Slicing.py
@@ -0,0 +1,9 @@
+texto="ABCDEFGHIJKLM"
+fragment1=texto[2:5]
+fragment2=texto[:5]
+fragment3=texto[2:10:2]#lee del 2 al 9 de dos en dos
+fragment4=texto[:-1]#lee alrevés: no considera la última letra
+print(fragment1)
+print(fragment2)
+print(fragment3)
+print(fragment4)
